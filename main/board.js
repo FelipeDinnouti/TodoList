bf2b607386912c78ac01ad7273ba61a8ba6584db
Falkun <fdinnouti@gmail.com>
2024-11-21 16:55:56 -0300

    Mudanças drásticas no CSS
    Organizei os arquivos CSS, deu uma leve reescrevida no código (não funciona completamente)
    E fiz com que cabesse multiplas boards na div center.
    Adicionar tasks nao funciona na primeira board por alguma razao

--- main/board.js
+++ main/board.js
@@ -1,7 +1,12 @@
 class Board {
-    constructor(title) {
+    constructor(title, index) {
         self.title = title
         self.task_list = []
+        self.index = index
+    }
+
+    getAsElement() {
+        return document.getElementById("board"+Number.toString(index));
     }
 
     checkboxChange(event) {
@@ -12,8 +17,23 @@ class Board {
         }
     }
 
-    createTask(description,date) {
-        self.task_list.push(new TaskInformation(description, date));
+    createTaskElement(add_to_list = true) {
+        const board_object = document.getElementById(`board${self.index}`);
+        const task_list = board_object.getElementsByClassName("task_list")[0];
+
+        let task_name_input = board_object.getElementsByClassName("task_name_input")[0];
+        let task_date_input = board_object.getElementsByClassName("task_date_input")[0];
+
+        let description = task_name_input.value;
+        let date = task_date_input.value;
+
+        if (description === "" || date === "") {
+            return; // Não cria se a descrição ou a data estiverem vazias
+        }
+
+        // Reseta o input
+        task_name_input.value = "";
+        task_date_input.value = "";
 
         // Criando a nova tarefa em HTML
         const task_object = document.createElement("li");
@@ -50,6 +70,63 @@ class Board {
          //Verificar se a tarefa está feita
         checkbox.addEventListener('change', this.checkboxChange);
 
-        return task_object // Retorna a tarefa completa
+        task_list.appendChild(task_object); // Retorna a tarefa completa
+        self.task_list.push(new TaskInformation());
+
+         //Verifica a data da tarefa
+        const hoje = new Date();
+        const dataHoje = hoje.toISOString().split('T')[0]; 
+
+        const tarefaData = date.split('T')[0]; 
+
+        if (tarefaData < dataHoje) {
+            console.log("tarefa atrasada");
+        } else if (tarefaData === dataHoje) {
+            console.log("tarefa para hoje");
+        } else {
+            console.log("tarefa dentro do prazo");
+        }
+    }
+
+    createBoardElement() {
+        const board_object = document.createElement("div");
+        const board_title = document.createElement("p");
+        const task_list = document.createElement("ul");
+        
+        const input = document.createElement("div");
+        const text_input = document.createElement("input");
+        const date_input = document.createElement("input");
+        const button = document.createElement("button");
+
+        board_object.className = "board";
+        board_object.id = `board${self.index}`
+        
+        board_title.className = "board_title";
+        board_title.innerText = self.title;
+
+        task_list.className = "task_list";
+
+        input.className = "input";
+
+        text_input.type = "text";
+        text_input.className = "task_name_input";
+
+        date_input.type = "date";
+        date_input.className = "task_date_input";
+        date_input.placeholder = "09092008";
+
+        button.className = "task_button";
+        button.innerText = "+";
+
+        board_object.appendChild(board_title);
+        board_object.appendChild(task_list);
+        board_object.appendChild(input);
+        input.appendChild(text_input);
+        input.appendChild(date_input);
+        input.appendChild(button);
+
+        button.addEventListener("click", this.createTaskElement);
+
+        return board_object;
     }
 }
--- main/index.js
+++ main/index.js
@@ -1,62 +1,19 @@
-const task_list = document.getElementById("board0");
-const add_button = document.getElementById("button");
-const task_name_input = document.getElementById("task_name_input");
-const task_date_input = document.getElementById("task_date_input");
+const board_list = document.getElementById("board_list");
 
 let boards = []; //Uma board é um conjunto de tasks
-boards.push(new Board("A Fazer"));
 
-function create_task_object(text, date) {
-  if (text === "" || date === "") {
-    return; // Não cria se a descrição ou a data estiverem vazias
-  }
+// Default Boards
 
-  // Reset the input
-  task_name_input.value = "";
-  task_date_input.value = "";
+var todo_board = new Board("A Fazer", 0);
 
-  // Adiciona o elemento a lista, criando o elemento na board 0
-  task_list.appendChild(boards[0].createTask(text, date));
+boards.push(todo_board);
+board_list.appendChild(todo_board.createBoardElement());
 
-  //Verifica a data da tarefa
-  const hoje = new Date();
-  const dataHoje = hoje.toISOString().split('T')[0]; 
+var doing_board = new Board("Fazendo", 1);
 
-  const tarefaData = date.split('T')[0]; 
+boards.push(doing_board);
+board_list.appendChild(doing_board.createBoardElement());
 
-  if (tarefaData < dataHoje) {
-      console.log("tarefa atrasada");
-  } else if (tarefaData === dataHoje) {
-      console.log("tarefa para hoje");
-  } else {
-      console.log("tarefa dentro do prazo");
-  }
-}
-
-add_button.onclick = function () {
-  create_task_object(task_name_input.value, task_date_input.value);
-};
-
-// Carregando Tasks
-
-// Logo antes de fechar a janela, salvar os dados
-window.addEventListener("beforeunload", function (e) {
-  localStorage.setItem(
-    "Boards",
-    JSON.stringify(boards),
-  );
-});
-
-// Assim que abrir a janela
-window.addEventListener("load", function (e) {
-  // lê uma lista de tasks salvas como uma string JSON e converte ela em Array
-  let task_list = JSON.parse(localStorage.getItem("Boards"));
-
-  // Itera por cada task salva e cria ela
-  for (let i = 0; i < task_list.length; i++) {
-    create_task_object(task_list[i].description, task_list[i].date);
-  }
-});
 
 // Completando tasks
 const checkboxes = document.querySelectorAll(".task_checkbox");
